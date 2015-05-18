1cab1355d02795aa13aa068e71b5fca90f78adb6
xxh <xxh@rock-chips.com>
2015-05-18 15:49:32 +0800

    apn file copy for RK3288 5.1 3G dongle

--- rk3288.mk
+++ rk3288.mk
@@ -26,6 +26,9 @@ PRODUCT_BRAND := Android
 PRODUCT_MODEL := rk3288
 PRODUCT_MANUFACTURER := rockchip
 
+# Get the long list of APNs
+PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/spn-conf.xml:system/etc/spn-conf.xml
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.version = 1.0.0 \
     ro.product.ota.host = www.rockchip.com:2300
