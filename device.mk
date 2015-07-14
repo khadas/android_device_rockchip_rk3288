ebb5ff6b56176bc348cc02741a42a78965d454ee
huangjc <huangjc@rock-chips.com>
2015-07-14 13:51:17 +0800

    box:support iso_operate for bd

--- device.mk
+++ device.mk
@@ -29,15 +29,14 @@ PRODUCT_PACKAGES += \
 #TARGET_USERIMAGES_USE_F2FS := true
 #endif
 
-PRODUCT_COPY_FILES += \
-    device/rockchip/rk3288/init.rc:root/init.rc
-
 ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
 PRODUCT_COPY_FILES += \
+    device/rockchip/rk3288/rk3288_box/init.rc:root/init.rc \
     device/rockchip/rk3288/fstab.rk30board.bootmode.unknown:root/fstab.rk30board.bootmode.unknown \
     device/rockchip/rk3288/rk3288_box/fstab.rk30board.bootmode.emmc:root/fstab.rk30board.bootmode.emmc
 else
   PRODUCT_COPY_FILES += \
+    device/rockchip/rk3288/init.rc:root/init.rc \
     device/rockchip/rk3288/fstab.rk30board.bootmode.unknown:root/fstab.rk30board.bootmode.unknown \
     device/rockchip/rk3288/fstab.rk30board.bootmode.emmc:root/fstab.rk30board.bootmode.emmc
 endif
