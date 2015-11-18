32e61641ea78741ca0c3fb81af1091f481f72b41
Hans Yang <yhx@rock-chips.com>
2015-11-18 11:11:11 +0800

    Do not copy fstab.rk30board.bootmode.emmc to fstab.rk30board
    Change-Id: I2428e2f5545d87422fb5eff91b771a0905f69d34
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- device.mk
+++ device.mk
@@ -40,7 +40,6 @@ PRODUCT_COPY_FILES += \
     device/rockchip/rk3288/rk3288_box/fstab.rk30board.bootmode.emmc:root/fstab.rk30board.bootmode.emmc
 else
   PRODUCT_COPY_FILES += \
-    device/rockchip/rk3288/fstab.rk30board.bootmode.emmc:root/fstab.rk30board \
     device/rockchip/rk3288/fstab.rk30board.bootmode.unknown:root/fstab.rk30board.bootmode.unknown \
     device/rockchip/rk3288/fstab.rk30board.bootmode.emmc:root/fstab.rk30board.bootmode.emmc
 endif
