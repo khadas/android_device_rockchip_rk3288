d3a2b5cf1523f28e0766e5fdfd3bf902ccc420c9
cw <cw@rock-chips.com>
2016-03-31 20:03:28 +0800

    Add TARGET_DEVICE for prebuild apps
    Change-Id: Id2e38142a7b037d000f1c8e4e437a85104687b42
    Signed-off-by: cw <cw@rock-chips.com>

--- buildspec-rk3288-box.mk
+++ buildspec-rk3288-box.mk
@@ -30,6 +30,10 @@ ifndef TARGET_PRODUCT
 TARGET_PRODUCT:=rk3288_box
 endif
 
+ifndef TARGET_DEVICE
+TARGET_DEVICE:=rk3288_box
+endif
+
 # Choose a variant to build.  If you don't pick one, the default is eng.
 # User is what we ship.  Userdebug is that, with a few flags turned on
 # for debugging.  Eng has lots of extra tools for development.
