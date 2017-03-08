827098564ccc6b723975b1c226711ba81ab2a887
huangjc <huangjc@rock-chips.com>
2017-03-08 11:24:01 +0800

    optimize from nougat:reserve CPU for the top app
    Change-Id: I5089512a5968ade3386004ddb4424e75a80105cd

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ else
     device/rockchip/rk3288/fstab.rk30board.bootmode.emmc:root/fstab.rk30board.bootmode.emmc
 endif
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.${TARGET_BOARD_PLATFORM_PRODUCT}.rc:root/init.${TARGET_BOARD_PLATFORM_PRODUCT}.rc
+
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
