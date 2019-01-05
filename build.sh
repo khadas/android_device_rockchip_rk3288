7da8486f61d026a91447581d9de024d0b03b85a3
Chen Jinsen <kevin.chen@rock-chips.com>
2019-01-05 17:10:18 +0800

    build.sh: update build config for android pie
    Change-Id: I573d9944b4548c376491ebd870311f114c7ad7c6
    Signed-off-by: Chen Jinsen <kevin.chen@rock-chips.com>

--- build.sh
+++ build.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 usage()
 {
-    echo "USAGE: [-o] [-u] [-v VERSION_NAME]"
-    echo "No ARGS means use default build option"
-    echo "WHERE: -o = generate ota package       "
-    echo "       -u = generate update.img        "
-    echo "       -v = set build variant"
+    echo "USAGE: [-U] [-K] [-A] [-p] [-o] [-u] [-v VERSION_NAME]  "
+    echo "No ARGS means use default build option                  "
+    echo "WHERE: -U = build uboot                                 "
+    echo "       -K = build kernel                                "
+    echo "       -A = build android                               "
+    echo "       -p = will build packaging in IMAGE      "
+    echo "       -o = build OTA package                           "
+    echo "       -u = build update.img                            "
+    echo "       -v = build android with 'user' or 'userdebug'    "
+    echo "       example: ./build.sh -U -K -A -u -v userdebug     "
     exit 1
 }
-
 BUILD_UBOOT=false
 BUILD_KERNEL=false
 BUILD_ANDROID=false
 BUILD_UPDATE_IMG=false
 BUILD_OTA=false
 BUILD_VARIANT=userdebug
+BUILD_PACKING=false
 
 # check pass argument
-while getopts "UKAouv:" arg
+while getopts "UKApouv:" arg
 do
     case $arg in
         U)
@@ -31,6 +36,10 @@ do
         A)
             echo "will build android"
             BUILD_ANDROID=true
+            ;;
+        p)
+            echo "will build packaging in IMAGE"
+            BUILD_PACKING=true
             ;;
         o)
             echo "will build ota package"
@@ -57,10 +66,9 @@ export PATH=$JAVA_HOME/bin:$PATH
 export CLASSPATH=.:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
 # source environment and chose target product
 DEVICE=`get_build_var TARGET_PRODUCT`
-UBOOT_DEFCONFIG=rk3288_secure_defconfig
+UBOOT_DEFCONFIG=rk3288
 KERNEL_DEFCONFIG=rockchip_defconfig
-KERNEL_DTS=rk3288-th804
-
+KERNEL_DTS=rk3288-evb-android-rk808-edp
 echo "KERNEL_DTS : $KERNEL_DTS"
 PACK_TOOL_DIR=RKTools/linux/Linux_Pack_Firmware
 IMAGE_PATH=rockdev/Image-$TARGET_PRODUCT
@@ -78,7 +86,7 @@ export STUB_PATCH_PATH=$STUB_PATH/PATCHES
 # build uboot
 if [ "$BUILD_UBOOT" = true ] ; then
 echo "start build uboot"
-cd u-boot && make distclean && make $UBOOT_DEFCONFIG && ./mkv7.sh && cd -
+cd u-boot && ./make.sh $UBOOT_DEFCONFIG && cd -
 if [ $? -eq 0 ]; then
     echo "Build uboot ok!"
 else
@@ -90,7 +98,7 @@ fi
 # build kernel
 if [ "$BUILD_KERNEL" = true ] ; then
 echo "Start build kernel"
-cd kernel && make clean && make ARCH=arm $KERNEL_DEFCONFIG && make ARCH=arm $KERNEL_DTS.img -j64 && cd -
+cd kernel && make clean && make ARCH=arm $KERNEL_DEFCONFIG && make ARCH=arm $KERNEL_DTS.img -j24 && cd -
 if [ $? -eq 0 ]; then
     echo "Build kernel ok!"
 else
@@ -102,50 +110,14 @@ fi
 # build android
 if [ "$BUILD_ANDROID" = true ] ; then
 echo "start build android"
-
-make installclean -j8
-make -j64
+make installclean
+make -j24
 if [ $? -eq 0 ]; then
     echo "Build android ok!"
 else
     echo "Build android failed!"
     exit 1
 fi
-fi
-
-if [ "$BUILD_VARIANT" = user ] ; then
-    if [ -e $PROJECT_TOP/build_back/system/build.prop ] ; then
-        echo "restore system/build.prop"
-        cp $PROJECT_TOP/build_back/system/build.prop $OUT/system/
-    else
-        echo "backup system/build.prop"
-        mkdir -p $PROJECT_TOP/build_back/system
-        cp $OUT/system/build.prop $PROJECT_TOP/build_back/system/build.prop
-    fi
-    if [ -e $PROJECT_TOP/build_back/system/etc/prop.default ] ; then
-        echo "restore system/etc/prop.default"
-        cp $PROJECT_TOP/build_back/system/etc/prop.default $OUT/system/etc/
-    else
-        echo "backup system/etc/prop.default"
-        mkdir -p $PROJECT_TOP/build_back/system/etc
-        cp $OUT/system/etc/prop.default $PROJECT_TOP/build_back/system/etc/prop.default
-    fi
-    if [ -e $PROJECT_TOP/build_back/vendor/build.prop ] ; then
-        echo "restore vendor/build.prop"
-        cp $PROJECT_TOP/build_back/vendor/build.prop $OUT/vendor/
-    else
-        echo "backup vendor/build.prop"
-        mkdir -p $PROJECT_TOP/build_back/vendor
-        cp $OUT/vendor/build.prop $PROJECT_TOP/build_back/vendor/build.prop
-    fi
-    if [ -e $PROJECT_TOP/build_back/vendor/default.prop ] ; then
-        echo "restore vendor/default.prop"
-        cp $PROJECT_TOP/build_back/vendor/default.prop $OUT/vendor/
-    else
-        echo "backup vendor/default.prop"
-        mkdir -p $PROJECT_TOP/build_back/vendor
-        cp $OUT/vendor/default.prop $PROJECT_TOP/build_back/vendor/default.prop
-    fi
 fi
 
 # mkimage.sh
@@ -171,42 +143,44 @@ fi
 
 # build update_image
 if [ "$BUILD_UPDATE_IMG" = true ] ; then
-	mkdir -p $PACK_TOOL_DIR/rockdev/Image/
-	cp -f $IMAGE_PATH/* $PACK_TOOL_DIR/rockdev/Image/
+    mkdir -p $PACK_TOOL_DIR/rockdev/Image/
+    cp -f $IMAGE_PATH/* $PACK_TOOL_DIR/rockdev/Image/
 
-	echo "Make update.img"
-	cd $PACK_TOOL_DIR/rockdev && ./mkupdate.sh
-	if [ $? -eq 0 ]; then
-		echo "Make update image ok!"
-	else
-		echo "Make update image failed!"
-		exit 1
-	fi
+    echo "Make update.img"
+    cd $PACK_TOOL_DIR/rockdev && ./mkupdate.sh
+    if [ $? -eq 0 ]; then
+        echo "Make update image ok!"
+    else
+        echo "Make update image failed!"
+        exit 1
+    fi
 
-	cd -
-	mv $PACK_TOOL_DIR/rockdev/update.img $IMAGE_PATH/
-	rm $PACK_TOOL_DIR/rockdev/Image -rf
+    cd -
+    mv $PACK_TOOL_DIR/rockdev/update.img $IMAGE_PATH/
+    rm $PACK_TOOL_DIR/rockdev/Image -rf
 fi
 
-mkdir -p $STUB_PATH
+# make and copy image
+if [ "$BUILD_PACKING" = true ] ; then
+    echo "make and copy packaging in IMAGE "
+    mkdir -p $STUB_PATH
 
-#Generate patches
-mkdir -p $STUB_PATCH_PATH && .repo/repo/repo forall -c "git diff > local_diff.patch" && (find . -name local_diff.patch -type f -size 0c| xargs rm) && (find . -path ./IMAGE -prune -o -name local_diff.patch | cpio -pdv $STUB_PATCH_PATH/)
-if [ -d $STUB_PATCH_PATH/IMAGE ]; then
-    rm $STUB_PATCH_PATH/IMAGE -rf
+    # Generate patches
+    .repo/repo/repo forall  -c "$PROJECT_TOP/device/rockchip/common/gen_patches_body.sh"
+
+    # Copy stubs
+    cp commit_id.xml $STUB_PATH/manifest_${DATE}.xml
+
+    mkdir -p $STUB_PATCH_PATH/kernel
+    cp kernel/.config $STUB_PATCH_PATH/kernel
+    cp kernel/vmlinux $STUB_PATCH_PATH/kernel
+
+    mkdir -p $STUB_PATH/IMAGES/
+    cp $IMAGE_PATH/* $STUB_PATH/IMAGES/
+    cp build.sh $STUB_PATH/build.sh
+
+    # Save build command info
+    echo "UBOOT:  defconfig: $UBOOT_DEFCONFIG" >> $STUB_PATH/build_cmd_info
+    echo "KERNEL: defconfig: $KERNEL_DEFCONFIG, dts: $KERNEL_DTS" >> $STUB_PATH/build_cmd_info
+    echo "ANDROID:$DEVICE-$BUILD_VARIANT" >> $STUB_PATH/build_cmd_info
 fi
-
-#Copy stubs
-cp commit_id.xml $STUB_PATH/manifest_${DATE}.xml
-
-mkdir -p $STUB_PATH/kernel
-cp kernel/.config $STUB_PATH/kernel
-cp kernel/vmlinux $STUB_PATH/kernel
-
-mkdir -p $STUB_PATH/IMAGES/
-cp $IMAGE_PATH/* $STUB_PATH/IMAGES/
-cp build.sh $STUB_PATH/build.sh
-#Save build command info
-echo "UBOOT:  defconfig: $UBOOT_DEFCONFIG" >> $STUB_PATH/build_cmd_info
-echo "KERNEL: defconfig: $KERNEL_DEFCONFIG, dts: $KERNEL_DTS" >> $STUB_PATH/build_cmd_info
-echo "ANDROID:$DEVICE-$BUILD_VARIANT" >> $STUB_PATH/build_cmd_info
